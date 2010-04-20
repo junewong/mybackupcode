25932b708cd913bb94810727b4d58494b95db49f
junewong12 <junewong12@05c7e10a-2281-11de-896d-b39e94f1936c>
2010-04-20 09:06:31 +0000

    fix something like filtering "." diretory.

--- windows_bat/cvs_commit_all/cvs_commit_all.bat
+++ windows_bat/cvs_commit_all/cvs_commit_all.bat
@@ -7,13 +7,13 @@ echo 进入文件夹：%1
 cd /d %1
 
 echo 尝试添加文件夹
-find . -type d | sed 's/\\\?CVS$//g' |sort |uniq -c |sed 's/^ \+//g' |grep  "^1" |cut -f 2 |xargs cvs add
+find . -type d | sed 's/\\\?CVS$//g' |sort |uniq -c |sed 's/^ \+//g' |grep  "^1" |cut -f 2 | grep -v "^\.$" | xargs cvs add
 
 echo 尝试添加二进制文件:
-cvs -n up -d  |grep "^? "  |cut -d " "  -f 2 |grep -E -v -i "Thumbs.db$" |grep -E -i "\.(jpg|jpeg|bmp|png|gif|zip|rar)$" |xargs cvs add -kb 
+cvs -n up -d  |grep "^? "  |cut -d " "  -f 2 |grep -E -v -i "Thumbs.db$" |grep -E -i "\.(jpg|jpeg|bmp|png|gif|zip|rar|swf|fla|swc|flv|mp3|wma|wmv|avi|rm|rmvb)$" |grep -v "^\.$" |xargs cvs add -kb 
 
 echo 尝试添加文本文件:
-cvs -n up -d  |grep "^? "  |cut -d " "  -f 2 |grep -E -v -i "Thumbs.db$" |xargs cvs add
+cvs -n up -d  |grep "^? "  |cut -d " "  -f 2 |grep -E -v -i "Thumbs.db$" |grep -v "^\.$" |xargs cvs add
 
 echo 尝试提交所有文件:
 cvs -n up -d  |grep -E "^(M|A) "  |cut -d " "  -f 2 |xargs echo  |cvs commit -F %log_file%
