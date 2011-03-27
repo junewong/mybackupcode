91ff57067bd6a0ac499407212150c73c3b3eb74c
junewong12 <junewong12@05c7e10a-2281-11de-896d-b39e94f1936c>
2011-03-27 14:37:09 +0000

    add comment

--- javascript/tools/fix_google_url.js
+++ javascript/tools/fix_google_url.js
@@ -1,1 +1,2 @@
+//change the url on google page as the real url withond google prefix.
 var a=document.getElementsByTagName('a');for(var i=0; i<a.length; i++){var d=a[i];if(d.className=='l'){d.onmousedown=function(){return true;};}};
